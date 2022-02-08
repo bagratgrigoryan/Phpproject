7da54d2257236f87cfaa483838c88b9bef89b794
bagratgrigoryan <bagratgrigoryan92@gmail.com>
2022-02-08 20:45:27 +0400

    user add

--- myAdmin/app/User.php
+++ myAdmin/app/User.php
@@ -16,24 +16,13 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'first_name',
+        'last_name',
+        'age',
+        'phone',
+        'email',
+        'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
